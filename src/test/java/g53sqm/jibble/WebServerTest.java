21193f7b0aecfca0dfe52c86f1c66d7febd38948
Arushad Ahmed <contact@arushad.org>
2015-11-29 17:01:06 +0800

    Fixed WebServer test case

--- src/test/java/g53sqm/jibble/WebServerTest.java
+++ src/test/java/g53sqm/jibble/WebServerTest.java
@@ -44,11 +44,11 @@ public class WebServerTest {
 	@Parameterized.Parameters
 	public static Collection primeNumbers() {		
 		return Arrays.asList(new Object[][] {
-			{ "Unable to determine the canonical path of the web root directory.", "missing_dir?:!*", 8080, "cgi-bin", "jibble.log", true },
+			{ "Unable to determine the canonical path of the web root directory.", "/><|:&missing_dir?:!*", 8080, "cgi-bin", "jibble.log", true },
 			{ "The specified root directory does not exist or is not a directory.", "missing_dir", 8080, "cgi-bin", "jibble.log", true },
-			{ "Unable to determine the canonical path of the cgi-bin directory.", "webfiles", 8080, "missing_dir?:!*", "jibble.log", true },
+			{ "Unable to determine the canonical path of the cgi-bin directory.", "webfiles", 8080, "/><|:&missing_dir?:!*", "jibble.log", true },
 			{ "The specified cgi-bin directory does not exist or is not a directory.", "webfiles", 8080, "missing_dir", "jibble.log", true },
-			{ "Unable to determine the canonical path of the log file.", "webfiles", 8080, "cgi-bin", "missing-file?:!*", true },
+			{ "Unable to determine the canonical path of the log file.", "webfiles", 8080, "cgi-bin", "/><|:&missing-file?:!*", true },
 			{ "The specified log file does not exist or is not a file.", "webfiles", 8080, "cgi-bin", "missing-file", true }
 		});
 	}
