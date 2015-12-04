851e610338b62f018c5c042e547e07ce7a361ebf
Arushad Ahmed <contact@arushad.org>
2015-12-04 22:17:34 +0800

    Fixed WebServerConfigConstructor test

--- src/test/java/g53sqm/jibble/WebServerConfigConstructorTest.java
+++ src/test/java/g53sqm/jibble/WebServerConfigConstructorTest.java
@@ -25,14 +25,16 @@ public class WebServerConfigConstructorTest {
 	private String configFile;
 	private boolean enableConsoleLogging;
 	private String expectedMessage;
+	private String actualMessage;
 	
 	@Rule
-	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+	public final static SystemOutRule systemOutRule = new SystemOutRule().enableLog();
 	
 	/**
 	 * Constructor
 	 */
-	public WebServerConfigConstructorTest(WebServerConfig config, String rootDir, int port, String configFile, String cgiBinDir, String logFile, boolean enableConsoleLogging, String expectedMessage) {
+	public WebServerConfigConstructorTest(WebServerConfig config, String rootDir, int port, String configFile,
+			String cgiBinDir, String logFile, boolean enableConsoleLogging, String expectedMessage, String actualMessage) {
 		this.config = config;
 		this.rootDir = rootDir;
 		this.port = port;
@@ -40,6 +42,7 @@ public class WebServerConfigConstructorTest {
 		this.logFile = logFile;
 		this.enableConsoleLogging = enableConsoleLogging;
 		this.expectedMessage = expectedMessage;
+		this.actualMessage = actualMessage;
 		this.configFile = configFile;
 	}
 	
@@ -47,12 +50,28 @@ public class WebServerConfigConstructorTest {
 	public static Collection primeNumbers() throws URISyntaxException {		
 		String file1 = Paths.get(WebServerConfigConstructorTest.class.getResource("/test-conf3.conf").toURI()).toString();
 		String file2 = Paths.get(WebServerConfigConstructorTest.class.getResource("/test-conf1.conf").toURI()).toString();
+		
+		WebServerConfig config1 = new WebServerConfig();
+		String msg1 = systemOutRule.getLogWithNormalizedLineSeparator(); systemOutRule.clearLog();
+		
+		WebServerConfig config2 = new WebServerConfig("./mydir", 80, "hello.conf", "bindir", "j.log", false);
+		String msg2 = systemOutRule.getLogWithNormalizedLineSeparator(); systemOutRule.clearLog();
+		
+		WebServerConfig config3 = new WebServerConfig("conf.missing");
+		String msg3 = systemOutRule.getLogWithNormalizedLineSeparator(); systemOutRule.clearLog();
+		
+		WebServerConfig config4 = new WebServerConfig(file1);
+		String msg4 = systemOutRule.getLogWithNormalizedLineSeparator(); systemOutRule.clearLog();
+		
+		WebServerConfig config5 = new WebServerConfig(file2);
+		String msg5 = systemOutRule.getLogWithNormalizedLineSeparator(); systemOutRule.clearLog();
+		
 		return Arrays.asList(new Object[][] {
-			{ new WebServerConfig(), "./webfiles", 8088, "./jibble.conf", "cgi-bin", "./jibble.log", true, "" },
-			{ new WebServerConfig("./mydir", 80, "hello.conf", "bindir", "j.log", false), "./mydir", 80, "hello.conf", "bindir", "j.log", false, ""  },
-			{ new WebServerConfig("conf.missing"), "./webfiles", 8088, "./jibble.conf", "cgi-bin", "./jibble.log", true, "Error: Config file conf.missing not found.\nWarning: Using default config settings.\n"  },
-			{ new WebServerConfig(file1), "./webfiles", 8088, file1, "cgi", "./jibble.log", false, "Error: Invalid port number in config file.\nWarning: Using default port 8088\n"  },
-			{ new WebServerConfig(file2), "./htdocs", 8000, file2, "cgi./scripts", "j.log", false, ""  },
+			{ config1, "./webfiles", 8088, "./jibble.conf", "cgi-bin", "./jibble.log", true, "", msg1 },
+			{ config2, "./mydir", 80, "hello.conf", "bindir", "j.log", false, "", msg2  },
+			{ config3, "./webfiles", 8088, "./jibble.conf", "cgi-bin", "./jibble.log", true, "Error: Config file conf.missing not found.\nWarning: Using default config settings.\n", msg3 },
+			{ config4, "./webfiles", 8088, file1, "cgi", "./jibble.log", false, "Error: Invalid port number in config file.\nWarning: Using default port 8088\n", msg4  },
+			{ config5, "./htdocs", 8000, file2, "cgi./scripts", "j.log", false, "", msg5  },
 		});
 	}
 	
@@ -88,7 +107,7 @@ public class WebServerConfigConstructorTest {
 	
 	@Test
 	public void testExpectedMessage() {
-		assertEquals(expectedMessage, systemOutRule.getLogWithNormalizedLineSeparator());			
+		assertEquals(expectedMessage, actualMessage);			
 	}
 	
 
